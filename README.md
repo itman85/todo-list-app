500371ba3ffa2929955ad0e26aa675efd27c4e1a
Phan Nguyen <phannguyen@nvg.vn>
2021-11-24 15:02:52 +0700

    create project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # todo-list-app
+this app will demo how to use android architecture components, clean architecture combine with mvvm, redux
---
+++ app/src/main/java/com/picoder/sample/todolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.picoder.sample.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
