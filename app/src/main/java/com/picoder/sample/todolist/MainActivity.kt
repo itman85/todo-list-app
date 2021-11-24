b1831dcac8d2ca785c7180bd5170c9a7dfd1f6ad
Phan Nguyen <phannguyen@nvg.vn>
2021-11-24 18:16:29 +0700

    create layout for add / update item

--- app/src/main/java/com/picoder/sample/todolist/MainActivity.kt
+++ app/src/main/java/com/picoder/sample/todolist/MainActivity.kt
@@ -2,10 +2,29 @@ package com.picoder.sample.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // will change action bar as fragment show
+        setupActionBarWithNavController(findNavigationController())
+    }
+
+    // handle click navigation up
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavigationController()
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun findNavigationController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        return navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/picoder/sample/todolist/fragment/list/ListFragment.kt
@@ -0,0 +1,30 @@
+package com.picoder.sample.todolist.fragment.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.picoder.sample.todolist.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+        return view
+    }
+
+}
