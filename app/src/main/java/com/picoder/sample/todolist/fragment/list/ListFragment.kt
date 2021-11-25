65369a104173be26baab25a67f59a4775251060b
Phan Nguyen <phannguyen@nvg.vn>
2021-11-25 18:07:36 +0700

    add menu and slide animation

--- app/src/main/java/com/picoder/sample/todolist/fragment/list/ListFragment.kt
+++ app/src/main/java/com/picoder/sample/todolist/fragment/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.picoder.sample.todolist.fragment.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.picoder.sample.todolist.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -24,7 +22,14 @@ class ListFragment : Fragment() {
         view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
     }
 
 }
